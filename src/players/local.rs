52966383f26a3b764bbd813bc7bb4d08e3a13e86
haruki7049 <tontonkirikiri@gmail.com>
2026-06-14 17:03:00 +0900

    Update

--- src/players/local.rs
+++ src/players/local.rs
@@ -1,3 +1,5 @@
+use std::{fs::File, io::BufReader};
+
 use super::AudioPlayer;
 use thiserror::Error;
 
@@ -5,12 +7,26 @@ use thiserror::Error;
 pub struct LocalAudioPlayer {}
 
 #[derive(Debug, Error)]
-pub enum LocalAudioPlayerError {}
+pub enum LocalAudioPlayerError {
+    #[error("Device sink error via rodio crate: {0:?}")]
+    RodioDeviceSink(#[from] rodio::DeviceSinkError),
+
+    #[error("Play error via rodio crate: {0:?}")]
+    RodioPlayError(#[from] rodio::stream::PlayError),
+
+    #[error("IO error via std crate: {0:?}")]
+    Io(#[from] std::io::Error),
+}
 
 impl AudioPlayer for LocalAudioPlayer {
     type Error = LocalAudioPlayerError;
 
     fn play(&self, filepath: &std::path::PathBuf) -> Result<(), Self::Error> {
-        todo!()
+        let sink_handle = rodio::DeviceSinkBuilder::open_default_sink()?;
+        let reader = BufReader::new(File::open(filepath)?);
+        let player = rodio::play(&sink_handle.mixer(), reader)?;
+        player.sleep_until_end();
+
+        Ok(())
     }
 }

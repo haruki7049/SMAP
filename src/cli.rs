a7e85ff28bf6682f65dac8a3a01b518b4ad23c54
haruki7049 <tontonkirikiri@gmail.com>
2026-06-14 16:40:41 +0900

    Update

--- src/cli.rs
+++ src/cli.rs
@@ -5,8 +5,11 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[clap(version, about, author)]
 pub struct CLIArgs {
+    /// A target IP.
+    /// If this is none, play back via your local device.
     #[arg(short, long)]
-    target: SocketAddr,
+    target: Option<SocketAddr>,
 
+    /// An audio file path
     file: PathBuf,
 }

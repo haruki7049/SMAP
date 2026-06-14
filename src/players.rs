ff91f06fe2e99ec2341534c9b5e3eda2c25855ac
haruki7049 <tontonkirikiri@gmail.com>
2026-06-14 20:45:02 +0900

    Update

--- src/players.rs
+++ src/players.rs
@@ -1,3 +1,6 @@
+use serde::Deserialize;
+use serde::Serialize;
+use std::net::SocketAddr;
 use std::path::Path;
 
 pub mod local;
@@ -8,6 +11,24 @@ pub trait AudioPlayer {
 
     fn play(&self, filepath: &Path) -> Result<(), Self::Error>;
     fn volume(&mut self, volume: Volume);
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Packet {
+    #[serde(with = "serde_bytes")]
+    pub bytes: Vec<u8>,
+    pub volume: Volume,
+    pub socket: SocketAddr,
+}
+
+impl Packet {
+    pub fn new(bytes: Vec<u8>, volume: Volume, socket: SocketAddr) -> Self {
+        Self {
+            bytes,
+            volume,
+            socket,
+        }
+    }
 }
 
 pub type Volume = f32;
--- src/players/remote.rs
+++ src/players/remote.rs
@@ -1,8 +1,12 @@
-use std::net::SocketAddr;
-use std::path::Path;
-
 use super::AudioPlayer;
+use super::Packet;
 use super::Volume;
+use std::fs::File;
+use std::io::Read;
+use std::io::Write;
+use std::net::SocketAddr;
+use std::net::TcpStream;
+use std::path::Path;
 use thiserror::Error;
 
 #[derive(Debug)]
@@ -18,13 +22,31 @@ impl RemoteAudioPlayer {
 }
 
 #[derive(Debug, Error)]
-pub enum RemoteAudioPlayerError {}
+pub enum RemoteAudioPlayerError {
+    #[error("IO error from std crate: {0}")]
+    Io(#[from] std::io::Error),
+
+    #[error("Serde encoding error from serde_json crate: {0}")]
+    SerdeEncoding(#[from] serde_json::Error),
+}
 
 impl AudioPlayer for RemoteAudioPlayer {
     type Error = RemoteAudioPlayerError;
 
     fn play(&self, filepath: &Path) -> Result<(), Self::Error> {
-        todo!()
+        let volume = self.volume;
+        let socket = self.socket;
+
+        let mut bytes: Vec<u8> = Vec::new();
+        File::open(filepath)?.read_to_end(&mut bytes)?;
+        let packet = Packet::new(bytes, volume, socket);
+
+        // Send packet struct to remote PC via TCP
+        let v: Vec<u8> = serde_json::to_vec(&packet)?;
+        let mut stream: TcpStream = TcpStream::connect(&socket)?;
+        stream.write_all(&v)?;
+
+        Ok(())
     }
 
     fn volume(&mut self, volume: Volume) {
